8b64001bc9be9a71be73a22814b569a1e42b7ac1
Chris Ballinger <chrisballinger@gmail.com>
2019-07-12 18:03:16 -0700

    Initial Commit

---
+++ BikeParty/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BikeParty
+//
+//  Created by Chris Ballinger on 7/12/19.
+//  Copyright © 2019 Chris Ballinger. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
